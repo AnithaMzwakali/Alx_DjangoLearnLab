b7744c2fbe24767b52f5abd585db690589fd8051
Anitha <anithamzwakali97@gmail.com>
2025-11-15 00:00:05 +0200

    Fix manage.py indentation and merge conflict markers

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,7 +14,6 @@ HEAD
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
