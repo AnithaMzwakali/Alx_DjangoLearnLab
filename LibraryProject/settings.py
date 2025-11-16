63cd93e986c5788e3fb728be2ca30d085e7b351a
Anitha Mzwakali <136008347+AnithaMzwakali@users.noreply.github.com>
2025-11-16 13:01:24 +0200

    Add accounts app to installed apps

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'advanced_features_and_security',
     'relationship_app',
     'bookshelf',
+    'accounts',
 ]
 
 AUTH_USER_MODEL = "advanced_features_and_security.CustomUser"
