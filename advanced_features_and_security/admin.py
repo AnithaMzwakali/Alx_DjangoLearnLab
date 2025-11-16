48708ef068f041b38cbe5c1ca588e1b1ec359112
Anitha Mzwakali <136008347+AnithaMzwakali@users.noreply.github.com>
2025-11-16 19:29:50 +0200

    Reorder fields in CustomUserAdmin display and search

--- advanced_features_and_security/admin.py
+++ advanced_features_and_security/admin.py
@@ -5,22 +5,22 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ("email", "username", "date_of_birth", "is_staff", "is_active")
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
     list_filter = ("is_staff", "is_active", "date_of_birth")
     fieldsets = (
         (None, {"fields": ("username", "email", "password")}),
         ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
-        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("username", "email", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active")}
-        ),
+            "fields": ("username", "email", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active"),
+        }),
     )
-    search_fields = ("email", "username")
-    ordering = ("email",)
+    search_fields = ("username", "email")
+    ordering = ("username",)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
